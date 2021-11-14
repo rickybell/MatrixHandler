2e8a8e12bc9da9c6a59a43d49328059c8cd0acff
henrique.belloube <henrique.belloube@gmail.com>
2021-11-14 19:31:55 -0300

    First version with matrix_handler tests works.

--- app/controllers/echos_controller.rb
+++ app/controllers/echos_controller.rb
@@ -1,33 +1,22 @@
 # EchosController class
 class EchosController < ApplicationController
   def show
-    # result = {
-    #   matrix_format: '',
-    #   invert: '',
-    #   flatten: '',
-    #   sum: '',
-    #   multiply: ''
-    # }
-
+    
     matrix_handler = MatrixHandler.new(params)
-    # matrix = matrix_handler.matrix
+    
+    unless matrix_handler.handlable?
 
-    render json: matrix_handler.errors, status: 500 unless matrix_handler.handlable?
+      result = {
+        matrix_format: matrix_handler.matrix_format,
+        invert: matrix_handler.invert,
+        flatten: matrix_handler.flatten,
+        sum: matrix_handler.sum,
+        multiply: matrix_handler.multiply
+      }
 
-    result = {
-      matrix_format: matrix_handler.matrix_format,
-      invert: matrix_handler.invert,
-      flatten: matrix_handler.flatten,
-      sum: matrix_handler.sum,
-      multiply: matrix_handler.multiply
-    }
-
-    # result[:matrix_format] = matrix.map { |row| row.join(',') }.join("\n")
-    # result[:flatten] = matrix.join(',')
-    # result[:invert] = matrix.transpose
-    # result[:sum] = matrix.flatten.sum
-    # result[:multiply] = matrix.flatten.inject(1, :*)
-
-    render json: result, status: :ok
+      render json: result, status: :ok
+    else
+      render json: matrix_handler.errors, status: 500  
+    end
   end
 end
